f4340ea58031744102e30a01e7bc196adcdd1d58
Ugur Öz <diyetisyenuguroz@gmail.com>
2022-03-30 14:58:09 +0200

    v8

---
+++ src/main/resources/db.migration/V1_0_0__initial_db_setup.sql
@@ -0,0 +1,3 @@
+create table authorities (username varchar(255) not null, authority varchar(255), primary key (username));
+create table users (username varchar(255) not null, enabled boolean not null, password varchar(255), authority_username varchar(255), primary key (username));
+alter table users add constraint FKgyqmryniy5ww2ajbsj7y71c9l foreign key (authority_username) references authorities;
